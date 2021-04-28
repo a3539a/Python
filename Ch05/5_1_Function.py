045c25c128a4e0b09a13e801908d63b5d875ef04
a3539a <a3539a@gmail.com>
2021-04-28 17:10:46 +0900

    2021/04/28 파이썬 Ch05 함수 실습예제

---
+++ Ch05/5_1_Function.py
@@ -0,0 +1,93 @@
+"""
+날짜 : 2021/04/28
+이름 : 김승용
+내용 : 파이썬 함수 실습하기 교재 p114
+
+함수(Function)
+    - 프로그래밍에서 일련의 로직 단위를 모듈로 만든것
+    - 함수는 정의, 호출로 이루어진다
+"""
+
+# 함수정의
+
+def f(x):
+    y = 2 * x + 3
+    return y
+
+# 함수 호출
+
+r1 = f(1)
+r2 = f(2)
+r3 = f(3)
+
+print('r1 : ', r1)
+print('r2 : ', r2)
+print('r3 : ', r3)
+
+# 함수의 유형1 - 매개변수 O, 리턴값 O
+
+def type1(x, y):
+    z = x + y
+    return z
+
+rs1 = type1(1, 2)
+rs2 = type1(3, 4)
+rs3 = type1(5, 6)
+
+print('rs1 : ', rs1)
+print('rs2 : ', rs2)
+print('rs3 : ', rs3)
+
+# 함수의 유형2 - 매개변수 O, 리턴값 X
+
+def type2(items):
+    total = 0
+
+    for item in items:
+        total += item
+
+    print('items 합 : ', total)
+
+type2([1, 2, 3, 4, 5])
+type2((2, 4, 6, 8, 10))
+
+# 함수의 유형3 - 매개변수 X, 리턴값 O
+
+def type3():
+    total = 0
+
+    for i in range(11):
+        total += i
+
+    return total
+
+result = type3()
+print('result : ', result)
+
+# 함수의 유형4 - 매개변수 X, 리턴값 X
+
+def type4():
+    print('type3 rsult : ', type3())
+
+type4()
+
+# 확인문제
+
+def gugudan(num):
+    print(num, '단')
+    # print('%d x 1 = {}'.format(num, num*1))
+    # print('%d x 2 = {}'.format(num, num*2))
+    # print('%d x 3 = {}'.format(num, num*3))
+    # print('%d x 4 = {}'.format(num, num*4))
+    # print('%d x 5 = {}'.format(num, num*5))
+    # print('%d x 6 = {}'.format(num, num*6))
+    # print('%d x 7 = {}'.format(num, num*7))
+    # print('%d x 8 = {}'.format(num, num*8))
+    # print('%d x 9 = {}'.format(num, num*9))
+
+    for i in range(1, 10):
+        print('{} x {} = {}'.format(num, i, num * i))
+
+gugudan(2)
+gugudan(3)
+gugudan(7)
